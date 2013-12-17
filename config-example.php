9fc5198ac2fe127b94eac3160013444201b8bfbd
Joachim <joachim.bachstatter@gmail.com>
2013-12-17 13:23:48 +0100

    config example

---
+++ config-example.php
@@ -0,0 +1,16 @@
+<?php 
+
+//remove example and put in your info
+
+date_default_timezone_set('Europe/Stockholm');
+
+define('ROOT_PATH', dirname(__FILE__));
+
+
+// Database configurations
+define('DB_HOST', 'db host');
+define('DB_NAME', 'db name');
+define('DB_USER', 'db name');
+define('DB_PASS', 'db pass');
+
+ ?>
