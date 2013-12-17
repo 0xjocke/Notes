4362d62c79fd65f55f71b6084c344dba0809b444
Joachim <joachim.bachstatter@gmail.com>
2013-12-17 13:53:32 +0100

    add small logo

--- partials/all.php
+++ partials/all.php
@@ -7,9 +7,11 @@
 		<h2>All notes</h2>
 		<ul>
 		<?php foreach ($notes as $note): ?>
-			<li class="specificNote" data-id="<?php echo $note->id ?>"><a><?php echo $note->title; ?></a></li>
+			<li class="specificNote" data-id="<?php echo $note->id ?>">
+				<img class="dateIcon" src="/images/date.svg" alt="date">
+				<a><?php echo $note->title; ?></a>
+			</li>
 		<?php endforeach; ?>
-			<li><a href="" class="smallBtn">Get more</a></li>
 		</ul>
 	</div>
 </div>
