8cb6cc3072bd1203298573bd0c133a104a3fd7e9
Joachim <joachim.bachstatter@gmail.com>
2014-01-17 14:41:43 +0100

    mobile should note scale

--- partials/header.php
+++ partials/header.php
@@ -3,6 +3,7 @@
 	<head>
 			<meta charset="utf-8">
 			<title>Notes</title>
+			<meta name="viewport" content="width=device-width, initial-scale=1">
 			<link rel="stylesheet" href="/css/style.css">
 			<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 			<script>
