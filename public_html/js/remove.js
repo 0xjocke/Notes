6d5b13e523391ddfdc6be9e2f1ca208de2da0580
Joachim <joachim.bachstatter@gmail.com>
2014-01-17 14:59:47 +0100

    use global function

--- public_html/js/remove.js
+++ public_html/js/remove.js
@@ -1,4 +1,3 @@
-/*global console:true*/
 /*global goToHome:true*/
 $(document).on('click', '.reallyDelete', function(event) {
    event.preventDefault();
@@ -16,35 +15,13 @@ $(document).on('click', '.reallyDelete', function(event) {
       .done(function(html) {
          $('.main').replaceWith(html);
          link.removeClass('loading');
-      })
-      .fail(function() {
-         console.log("error");
-      })
-      .always(function() {
-         console.log("complete");
+         goToHome();
       });
-   goToHome();
 });
 
 $(document).on('click', '.cancel', function(event) {
    event.preventDefault();
    var link = $(this);
    link.addClass('loading');
-   
-   $.ajax({
-      type: 'GET',
-      url: '/mainContainer.php',
-      data: {'pageId': 'home.php'}
-   })
-    // when done, go to home
-   .done(function(html) {
-         $('.main').replaceWith(html);
-         link.removeClass('loading');
-   })
-   .fail(function() {
-      console.log("error");
-   })
-   .always(function() {
-      console.log("complete");
-   });
+   goToHome();
 });
