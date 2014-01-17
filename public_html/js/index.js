fc3e6adf2b72ead252edcedbc104ee3b13c5dba7
Joachim <joachim.bachstatter@gmail.com>
2014-01-17 14:59:13 +0100

    using global functions

--- public_html/js/index.js
+++ public_html/js/index.js
@@ -1,75 +1,24 @@
+/*global goToPage:true*/
+/*global getNote:true*/
 // on click show clicked note
 $(document).on('click', '.indexSpecificNote', function(event) {
    event.preventDefault();
-   // send the url plus category id with get
    var noteId = $(this).attr('data-id');
-
-   $.ajax({
-         type: 'GET',
-         url: '/note.php',
-         data: {'noteId': noteId}
-         })
-   // when done, replace portfolio section with the 
-   .done(function(html) {
-      $('.note').replaceWith(html);
-   });
+   getNote(noteId);
 });
 
-// remove
+// Go to remove page
 $(document).on('click', '.remove', function(event) {
    event.preventDefault();
-   // save attrbute to noteId
    var noteId = $(this).attr('data-id');
    var link = $(this);
-   link.addClass('loading');
-   // go to remove page
-   $.ajax({
-         type: 'GET',
-         url: '/mainContainer.php',
-         data: {'pageId': 'remove.php'}
-            })
-            // when done, replace portfolio section with the 
-   .done(function(html) {
-         $('.main').replaceWith(html);
-         link.removeClass('loading');
-   });
-   // send the url plus category id with get
-   $.ajax({
-         type: 'GET',
-         url: '/remove.php',
-         data: {'noteId': noteId}
-            })
-   // when done, Put in the right note name and id
-   .done(function(html) {
-      $('.main').replaceWith(html);
-   });
+  goToPage('remove', noteId, link);
+  
 });
-
+//go to edit page
 $(document).on('click', '.edit', function(event) {
    event.preventDefault();
-   // save attrbute to noteId
    var noteId = $(this).attr('data-id');
    var link = $(this);
-   link.addClass('loading');
-   // go to remove page
-   $.ajax({
-         type: 'GET',
-         url: '/mainContainer.php',
-         data: {'pageId': 'edit.php'}
-            })
-            // when done, replace portfolio section with the 
-   .done(function(html) {
-         $('.main').replaceWith(html);
-         link.removeClass('loading');
-   });
-   // send the url plus category id with get
-   $.ajax({
-         type: 'GET',
-         url: '/edit.php',
-         data: {'noteId': noteId}
-            })
-   // when done, replace portfolio section with the 
-   .done(function(html) {
-      $('.main').replaceWith(html);
-   });
+   goToPage('edit', noteId, link);
 });
