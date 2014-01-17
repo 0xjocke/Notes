f835aef895d6ebf52297b37d1221bebf463e7dd2
Joachim <joachim.bachstatter@gmail.com>
2014-01-17 14:59:01 +0100

    Global functions

--- public_html/js/functions.js
+++ public_html/js/functions.js
@@ -1,4 +1,3 @@
-  /*global console:true*/
   function goToHome(){
   $.ajax({
          type: 'GET',
@@ -8,11 +7,35 @@
             // when done, replace portfolio section with the 
    .done(function(html) {
          $('.main').replaceWith(html);
-   })
-   .fail(function() {
-      console.log("error");
-   })
-   .always(function() {
-      console.log("complete");
+   });
+}
+
+function goToPage(pageName, noteId, link){
+   if (link) {
+    link.addClass('loading');
+   }
+    // send the url plus category id with get
+   $.ajax({
+         type: 'GET',
+         url: '/'+pageName+'.php',
+         data: {'noteId': noteId}
+         })
+   // when done, Put in the right note name and id
+   .done(function(html) {
+      $('.main').replaceWith(html);
+      if (link) {
+        link.removeClass('loading');
+      }
+   });
+}
+function getNote(noteId) {
+     $.ajax({
+         type: 'GET',
+         url: '/note.php',
+         data: {'noteId': noteId}
+         })
+   // when done, replace portfolio section with the 
+   .done(function(html) {
+      $('.note').replaceWith(html);
    });
 }
